97f4774c2b4cf7219388bea00e615da818aeccaf
Megha Gupta <meghagupta3234@gmail.com>
2024-02-01 13:46:14 +0530

    updated config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "https://gmegha3234.github.io/tic-tac-toe/",
   plugins: [react()],
 })
